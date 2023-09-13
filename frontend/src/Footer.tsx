5e012ec0f40aba3d0956d78eb82e90c833db0a34
Sahil Kapur <sahilsk11@users.noreply.github.com>
2023-09-13 17:29:34 -0400

    footer (#4)
    * .
    
    * footer
    
    * .

---
+++ frontend/src/Footer.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import "./app.css";
+import "./footer.css";
+import { endpoint } from "./App";
+
+export default function StatsFooter() {
+  const [uniqueUsers, setUniqueUsers] = useState<number | null>(null);
+  const [backtests, setBacktests] = useState<number | null>(null);
+  const [strategies, setStrategies] = useState<number | null>(null);
+
+  async function getStats() {
+    try {
+      const response = await fetch(endpoint + "/usageStats");
+      const responseJson = await response.json()
+      setUniqueUsers(responseJson.uniqueUsers)
+      setBacktests(responseJson.backtests)
+      setStrategies(responseJson.strategies)
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    const timer = setTimeout(() => getStats(), 5000)
+    getStats()
+    return () => clearTimeout(timer);
+  }, []);
+
+  return <>
+    <div className="footer">
+      <div className="footer-text-wrapper">
+        <ul>
+          {/* <li className="footer-stat">Launch Date: 09/12/2023</li> */}
+          <li className="footer-stat">Unique Users: {uniqueUsers}</li>
+          <li className="footer-stat"># Backtests Run: {backtests}</li>
+          <li className="footer-stat">Distinct Strategies Tested: {strategies}</li>
+          <li className="footer-stat">Made with ❤️ in NYC</li>
+        </ul>
+      </div>
+    </div>
+  </>
+}
