e66337329fbec4cdaee99e651e38bde167e0032e
sahilsk11 <sahilkapur.a@gmail.com>
2024-06-17 01:39:19 -0400

    down

--- migrations/000010_multiple_universes.down.sql
+++ migrations/000010_multiple_universes.down.sql
@@ -4,3 +4,12 @@ drop table asset_universe;
 
 drop type asset_universe_name;
 
+alter table ticker
+rename ticker_id to universe_id;
+
+alter table ticker
+rename to universe;
+
+-- dirty, sorry
+alter table universe
+add column serial not null;
