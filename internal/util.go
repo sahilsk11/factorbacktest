b07d710c70ef7bd7757d528c4278ebc1f082f265
sahilsk11 <sahilkapur.a@gmail.com>
2024-06-25 15:28:29 -0400

    secrets

--- internal/util.go
+++ internal/util.go
@@ -15,12 +15,36 @@ func Pprint(i interface{}) {
 }
 
 type Secrets struct {
-	DataJockeyApiKey string `json:"dataJockey"`
-	ChatGPTApiKey    string `json:"gpt"`
+	DataJockeyApiKey string    `json:"dataJockey"`
+	ChatGPTApiKey    string    `json:"gpt"`
+	Db               DbSecrets `json:"db"`
+}
+
+type DbSecrets struct {
+	Host      string `json:"host"`
+	User      string `json:"user"`
+	Port      string `json:"port"`
+	Password  string `json:"password"`
+	Database  string `json:"database"`
+	EnableSsl bool   `json:"enableSsl"`
+}
+
+func (t DbSecrets) ToConnectionStr() string {
+	x := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s",
+		t.Host, t.Port, t.User, t.Password, t.Database)
+
+	if !t.EnableSsl {
+		x += " sslmode=disable"
+	}
+	return x
 }
 
 func LoadSecrets() (*Secrets, error) {
-	f, err := os.ReadFile("secrets.json")
+	secretsFile := "secrets.json"
+	if os.Getenv("ALPHA_ENV") == "dev" {
+		secretsFile = "secrets-dev.json"
+	}
+	f, err := os.ReadFile(secretsFile)
 	if err != nil {
 		return nil, fmt.Errorf("could not open secrets.json: %w", err)
 	}
