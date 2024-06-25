64c67bcd4431e3d11f6d54e2bf9d443c0c064788
sahilsk11 <sahilkapur.a@gmail.com>
2024-06-25 15:28:33 -0400

    add universe "all"

--- migrations/000011_unique_universe.up.sql
+++ migrations/000011_unique_universe.up.sql
@@ -1,6 +1,8 @@
 alter table asset_universe
 ADD CONSTRAINT unique_asset_universe_name UNIQUE (asset_universe_name);
 
+alter type asset_universe_name add value 'ALL';
+
 -- INSERT INTO asset_universe_ticker (ticker_id, asset_universe_id)
 -- SELECT t.ticker_id, au.asset_universe_id
 -- FROM ticker t
