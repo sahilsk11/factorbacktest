a94aef507e0c7c3f419717125e9d9d31b08ce7fb
sahilsk11 <sahilkapur.a@gmail.com>
2024-07-13 14:55:54 -0400

    .

--- internal/service/l2/factor_expression.service.go
+++ internal/service/l2/factor_expression.service.go
@@ -9,12 +9,14 @@ import (
 	"factorbacktest/internal/domain"
 	"factorbacktest/internal/repository"
 	l1_service "factorbacktest/internal/service/l1"
-
-	// "factorbacktest/internal/service"
+	"math"
 
 	"fmt"
 	"sync"
 	"time"
+
+	"github.com/go-jet/jet/v2/qrm"
+	"github.com/maja42/goval"
 )
 
 type ScoresResultsOnDay struct {
@@ -31,14 +33,14 @@ type FactorExpressionService interface {
 
 type factorExpressionServiceHandler struct {
 	Db                    *sql.DB
-	FactorMetricsHandler  FactorMetricCalculations
+	FactorMetricsHandler  factorMetricCalculations
 	PriceService          l1_service.PriceService
 	FactorScoreRepository repository.FactorScoreRepository
 }
 
 func NewFactorExpressionService(
 	db *sql.DB,
-	factorMetricsHandler FactorMetricCalculations,
+	factorMetricsHandler factorMetricCalculations,
 	priceService l1_service.PriceService,
 	factorScoreRepository repository.FactorScoreRepository,
 ) FactorExpressionService {
@@ -59,7 +61,7 @@ type workInput struct {
 type workResult struct {
 	Date             time.Time
 	Ticker           model.Ticker
-	ExpressionResult *ExpressionResult
+	ExpressionResult *expressionResult
 	Err              error
 	elapsedMs        int64
 	span             *domain.Span
@@ -155,7 +157,7 @@ func (h factorExpressionServiceHandler) CalculateFactorScores(ctx context.Contex
 					start := time.Now()
 					span, endSpan := domain.NewSpan(fmt.Sprintf("evaluating expression for %s on %s", input.Ticker.Symbol, input.Date.Format(time.DateOnly)))
 					subProfile, endProfile := span.NewSubProfile()
-					res, err := EvaluateFactorExpression(
+					res, err := evaluateFactorExpression(
 						context.WithValue(ctx, domain.ContextProfileKey, subProfile),
 						h.Db,
 						cache,
@@ -215,7 +217,7 @@ func (h factorExpressionServiceHandler) CalculateFactorScores(ctx context.Contex
 			Date:                 res.Date,
 		}
 
-		if res.Err != nil && !errors.As(res.Err, &FactorMetricsMissingDataError{}) {
+		if res.Err != nil && !errors.As(res.Err, &factorMetricsMissingDataError{}) {
 			out[res.Date].Errors = append(out[res.Date].Errors, res.Err)
 			errString := res.Err.Error()
 			m.Error = &errString
@@ -250,7 +252,7 @@ func (h factorExpressionServiceHandler) loadPriceCache(ctx context.Context, in [
 		Stdevs: []l1_service.LoadStdevCacheInput{},
 	}
 	for _, n := range in {
-		_, err := EvaluateFactorExpression(
+		_, err := evaluateFactorExpression(
 			ctx,
 			nil,
 			nil,
@@ -340,4 +342,387 @@ func (h factorExpressionServiceHandler) CalculateFactorScoresOnDay(ctx context.C
 		return nil, fmt.Errorf("scores missing from result: %w", err)
 	}
 	return r, nil
+}
+
+// combined everything related to factor expressions into this one file
+// good luck haha
+
+func constructFunctionMap(
+	ctx context.Context,
+	db *sql.DB,
+	pr *l1_service.PriceCache,
+	symbol string,
+	h factorMetricCalculations,
+	debug formulaDebugger,
+	currentDate time.Time,
+) map[string]goval.ExpressionFunction {
+	return map[string]goval.ExpressionFunction{
+		// we could break this up
+
+		// helper functions
+		"addDate": func(args ...interface{}) (interface{}, error) {
+			// addDate(date, years, months, days)
+			if len(args) < 4 {
+				return 0, fmt.Errorf("addDate needs needed 4 args, got %d", len(args))
+			}
+			date, err := time.Parse(time.DateOnly, args[0].(string))
+			if err != nil {
+				return 0, err
+			}
+
+			var years, months, days = args[1].(int), args[2].(int), args[3].(int)
+
+			date = date.AddDate(years, months, days)
+
+			return date.Format(time.DateOnly), nil
+		},
+
+		"nDaysAgo": func(args ...interface{}) (interface{}, error) {
+			if len(args) < 1 {
+				return 0, fmt.Errorf("nDaysAgo needs needed 1 arg, got %d", len(args))
+			}
+			n := args[0].(int)
+			d := currentDate.AddDate(0, 0, -n)
+
+			return d.Format(time.DateOnly), nil
+		},
+		"nMonthsAgo": func(args ...interface{}) (interface{}, error) {
+			if len(args) < 1 {
+				return 0, fmt.Errorf("nMonthsAgo needs needed 1 arg, got %d", len(args))
+			}
+			n := args[0].(int)
+			d := currentDate.AddDate(0, -n, 0)
+
+			return d.Format(time.DateOnly), nil
+		},
+		"nYearsAgo": func(args ...interface{}) (interface{}, error) {
+			if len(args) < 1 {
+				return 0, fmt.Errorf("nYearsAgo needs needed 1 arg, got %d", len(args))
+			}
+			n := args[0].(int)
+			d := currentDate.AddDate(-n, 0, 0)
+
+			return d.Format(time.DateOnly), nil
+		},
+
+		// metric functions
+
+		// price(date strDate)
+		"price": func(args ...interface{}) (interface{}, error) {
+			if len(args) < 1 {
+				return 0, fmt.Errorf("price needs needed 1 arg, got %d", len(args))
+			}
+			date, err := time.Parse(time.DateOnly, args[0].(string))
+			if err != nil {
+				return 0, err
+			}
+			p, err := h.Price(pr, symbol, date)
+			if err != nil {
+				return 0, err
+			}
+
+			debug.Add("price", p)
+
+			return p, nil
+		},
+
+		// pricePercentChange(start, end strDate)
+		"pricePercentChange": func(args ...interface{}) (interface{}, error) {
+			if len(args) < 2 {
+				return 0, fmt.Errorf("pricePercentChange needs needed 2 args, got %d", len(args))
+			}
+			start, err := time.Parse(time.DateOnly, args[0].(string))
+			if err != nil {
+				return 0, err
+			}
+			end, err := time.Parse(time.DateOnly, args[1].(string))
+			if err != nil {
+				return 0, err
+			}
+
+			p, err := h.PricePercentChange(pr, symbol, start, end)
+			if err != nil {
+				return 0, err
+			}
+
+			debug.Add("pricePercentChange", p)
+
+			return p, nil
+		},
+
+		// stdev(start, end strDate)
+		"stdev": func(args ...interface{}) (interface{}, error) {
+			if len(args) < 2 {
+				return 0, fmt.Errorf("stdev needs needed 2 args, got %d", len(args))
+			}
+
+			start, err := time.Parse(time.DateOnly, args[0].(string))
+			if err != nil {
+				return 0, err
+			}
+			end, err := time.Parse(time.DateOnly, args[1].(string))
+			if err != nil {
+				return 0, err
+			}
+
+			p, err := h.AnnualizedStdevOfDailyReturns(ctx, pr, symbol, start, end)
+			if err != nil {
+				return 0, err
+			}
+			debug.Add("stdev", p)
+
+			return p, nil
+		},
+		"marketCap": func(args ...interface{}) (interface{}, error) {
+			if len(args) < 1 {
+				return 0, fmt.Errorf("marketCap needs needed 1 arg, got %d", len(args))
+			}
+
+			date, err := time.Parse(time.DateOnly, args[0].(string))
+			if err != nil {
+				return 0, err
+			}
+
+			return h.MarketCap(db, symbol, date)
+		},
+		"pbRatio": func(args ...interface{}) (interface{}, error) {
+			if len(args) < 1 {
+				return 0, fmt.Errorf("pbRatio needs needed 1 arg, got %d", len(args))
+			}
+
+			date, err := time.Parse(time.DateOnly, args[0].(string))
+			if err != nil {
+				return 0, err
+			}
+
+			return h.PbRatio(db, symbol, date)
+		},
+		"peRatio": func(args ...interface{}) (interface{}, error) {
+			if len(args) < 1 {
+				return 0, fmt.Errorf("peRatio needs needed 1 arg, got %d", len(args))
+			}
+
+			date, err := time.Parse(time.DateOnly, args[0].(string))
+			if err != nil {
+				return 0, err
+			}
+
+			return h.PeRatio(db, symbol, date)
+		},
+	}
+}
+
+type expressionResult struct {
+	Value  float64
+	Reason formulaDebugger
+}
+
+func evaluateFactorExpression(
+	ctx context.Context,
+	db *sql.DB,
+	pr *l1_service.PriceCache,
+	expression string,
+	symbol string,
+	factorMetricsHandler factorMetricCalculations,
+	date time.Time, // expressions are evaluated on the given date
+) (*expressionResult, error) {
+	eval := goval.NewEvaluator()
+	variables := map[string]interface{}{
+		"currentDate": date.Format(time.DateOnly),
+	}
+
+	debug := formulaDebugger{}
+	functions := constructFunctionMap(ctx, db, pr, symbol, factorMetricsHandler, debug, date)
+	result, err := eval.Evaluate(expression, variables, functions)
+	if err != nil {
+		return nil, fmt.Errorf("failed to evaluate factor expression: %w", err)
+	}
+
+	// TODO - if it's a dry-run, we're not computing real results
+	// and should allow any value to be processed here
+	r, ok := result.(float64)
+	if !ok {
+		return nil, fmt.Errorf("failed to convert to float")
+	} else if math.IsNaN(r) {
+		return nil, fmt.Errorf("calculated NaN as expression result")
+	} else if math.IsInf(r, 0) {
+		return nil, fmt.Errorf("calculated infinity as expression result")
+	}
+
+	return &expressionResult{
+		Value:  r,
+		Reason: debug,
+	}, nil
+}
+
+type formulaDebugger map[string][]float64
+
+func (f formulaDebugger) Add(fieldName string, value float64) {
+	if _, ok := f[fieldName]; !ok {
+		f[fieldName] = make([]float64, 0)
+	}
+	f[fieldName] = append(f[fieldName], value)
+}
+
+type factorMetricsMissingDataError struct {
+	Err error
+}
+
+func (e factorMetricsMissingDataError) Error() string {
+	return e.Err.Error()
+}
+
+type factorMetricCalculations interface {
+	Price(pr *l1_service.PriceCache, symbol string, date time.Time) (float64, error)
+	PricePercentChange(pr *l1_service.PriceCache, symbol string, start, end time.Time) (float64, error)
+	AnnualizedStdevOfDailyReturns(ctx context.Context, pr *l1_service.PriceCache, symbol string, start, end time.Time) (float64, error)
+	MarketCap(tx qrm.Queryable, symbol string, date time.Time) (float64, error)
+	PeRatio(tx qrm.Queryable, symbol string, date time.Time) (float64, error)
+	PbRatio(tx qrm.Queryable, symbol string, date time.Time) (float64, error)
+}
+
+type factorMetricsHandler struct {
+	// both dependencies should be wrapped in some mds service
+	AdjustedPriceRepository     repository.AdjustedPriceRepository
+	AssetFundamentalsRepository repository.AssetFundamentalsRepository
+}
+
+func NewFactorMetricsHandler(adjPriceRepository repository.AdjustedPriceRepository, afRepository repository.AssetFundamentalsRepository) factorMetricCalculations {
+	return factorMetricsHandler{
+		AdjustedPriceRepository:     adjPriceRepository,
+		AssetFundamentalsRepository: afRepository,
+	}
+}
+
+type DryRunFactorMetricsHandler struct {
+	// these may contain duplicates
+	Prices []l1_service.LoadPriceCacheInput
+	Stdevs []l1_service.LoadStdevCacheInput
+}
+
+func (h *DryRunFactorMetricsHandler) Price(pr *l1_service.PriceCache, symbol string, date time.Time) (float64, error) {
+	h.Prices = append(h.Prices, l1_service.LoadPriceCacheInput{
+		Date:   date,
+		Symbol: symbol,
+	})
+	return 0, nil
+}
+
+func (h *DryRunFactorMetricsHandler) PricePercentChange(pr *l1_service.PriceCache, symbol string, start, end time.Time) (float64, error) {
+	h.Prices = append(h.Prices, l1_service.LoadPriceCacheInput{
+		Date:   start,
+		Symbol: symbol,
+	})
+	h.Prices = append(h.Prices, l1_service.LoadPriceCacheInput{
+		Date:   end,
+		Symbol: symbol,
+	})
+
+	return 1, nil
+}
+
+func (h *DryRunFactorMetricsHandler) AnnualizedStdevOfDailyReturns(ctx context.Context, pr *l1_service.PriceCache, symbol string, start, end time.Time) (float64, error) {
+	h.Stdevs = append(h.Stdevs, l1_service.LoadStdevCacheInput{
+		Start:  start,
+		End:    end,
+		Symbol: symbol,
+	})
+	return 1, nil
+}
+
+func (h *DryRunFactorMetricsHandler) MarketCap(tx qrm.Queryable, symbol string, date time.Time) (float64, error) {
+	return 1, nil
+}
+
+func (h *DryRunFactorMetricsHandler) PeRatio(tx qrm.Queryable, symbol string, date time.Time) (float64, error) {
+	return 1, nil
+}
+
+func (h *DryRunFactorMetricsHandler) PbRatio(tx qrm.Queryable, symbol string, date time.Time) (float64, error) {
+	return 1, nil
+}
+
+func (h factorMetricsHandler) Price(pr *l1_service.PriceCache, symbol string, date time.Time) (float64, error) {
+	return pr.Get(symbol, date)
+}
+
+func (h factorMetricsHandler) PricePercentChange(pr *l1_service.PriceCache, symbol string, start, end time.Time) (float64, error) {
+	startPrice, err := pr.Get(symbol, start)
+	if err != nil {
+		return 0, err
+	}
+
+	endPrice, err := pr.Get(symbol, end)
+	if err != nil {
+		return 0, err
+	}
+
+	return percentChange(endPrice, startPrice), nil
+}
+
+func percentChange(end, start float64) float64 {
+	return ((end - start) / end) * 100
+}
+
+func (h factorMetricsHandler) AnnualizedStdevOfDailyReturns(ctx context.Context, pr *l1_service.PriceCache, symbol string, start, end time.Time) (float64, error) {
+	return pr.GetStdev(ctx, symbol, start, end)
+}
+
+func (h factorMetricsHandler) MarketCap(tx qrm.Queryable, symbol string, date time.Time) (float64, error) {
+	out, err := h.AssetFundamentalsRepository.Get(tx, symbol, date)
+	if err != nil {
+		return 0, factorMetricsMissingDataError{err}
+	}
+
+	price, err := h.AdjustedPriceRepository.Get(symbol, date)
+	if err != nil {
+		return 0, err
+	}
+	if out.SharesOutstandingBasic == nil {
+		return 0, factorMetricsMissingDataError{fmt.Errorf("%s does not have # shares outstanding on %v", symbol, date)}
+	}
+
+	return *out.SharesOutstandingBasic * price, nil
+}
+
+func (h factorMetricsHandler) PeRatio(tx qrm.Queryable, symbol string, date time.Time) (float64, error) {
+	price, err := h.AdjustedPriceRepository.Get(symbol, date)
+	if err != nil {
+		return 0, err
+	}
+
+	out, err := h.AssetFundamentalsRepository.Get(tx, symbol, date)
+	if err != nil {
+		return 0, factorMetricsMissingDataError{err}
+	}
+	if out.EpsBasic == nil {
+		return 0, factorMetricsMissingDataError{fmt.Errorf("%s does not have eps on %v", symbol, date)}
+	}
+
+	return price / *out.EpsBasic, nil
+
+}
+
+func (h factorMetricsHandler) PbRatio(tx qrm.Queryable, symbol string, date time.Time) (float64, error) {
+	price, err := h.AdjustedPriceRepository.Get(symbol, date)
+	if err != nil {
+		return 0, err
+	}
+
+	out, err := h.AssetFundamentalsRepository.Get(tx, symbol, date)
+	if err != nil {
+		return 0, factorMetricsMissingDataError{err}
+	}
+
+	if out.TotalAssets == nil {
+		return 0, factorMetricsMissingDataError{fmt.Errorf("missing total assets")}
+	}
+	if out.TotalLiabilities == nil {
+		return 0, factorMetricsMissingDataError{fmt.Errorf("missing total liabilities")}
+	}
+	if out.SharesOutstandingBasic == nil {
+		return 0, factorMetricsMissingDataError{fmt.Errorf("missing shares outstanding")}
+	}
+
+	return price / ((*out.TotalAssets - *out.TotalLiabilities) / *out.SharesOutstandingBasic), nil
 }
