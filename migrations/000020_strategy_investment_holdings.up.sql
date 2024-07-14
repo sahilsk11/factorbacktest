97ee7acaf38bc4b5cb513f15ea296bd92bc1c526
sahilsk11 <sahilkapur.a@gmail.com>
2024-07-13 23:18:53 -0400

    .

--- migrations/000020_strategy_investment_holdings.up.sql
+++ migrations/000020_strategy_investment_holdings.up.sql
@@ -9,3 +9,30 @@ create table strategy_investment_holdings(
 alter table strategy_investment add column end_date date;
 
 insert into ticker (symbol, name) values (':CASH', 'cash');
+
+CREATE VIEW latest_strategy_investment_holdings AS
+WITH ranked_holdings AS (
+    SELECT
+        sih.strategy_investment_holdings_id,
+        sih.strategy_investment_id,
+        sih.date,
+        sih.ticker,
+        sih.quantity,
+        t.symbol,
+        ROW_NUMBER() OVER (PARTITION BY sih.strategy_investment_id ORDER BY sih.date DESC) AS rnk
+    FROM
+        strategy_investment_holdings sih
+    JOIN
+        ticker t ON sih.ticker = t.ticker_id
+)
+SELECT
+    strategy_investment_holdings_id,
+    strategy_investment_id,
+    date,
+    ticker,
+    quantity,
+    symbol
+FROM
+    ranked_holdings
+WHERE
+    rnk = 1;
