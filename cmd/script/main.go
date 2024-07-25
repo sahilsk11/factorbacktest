66f3c20b2e0a612632bb2d377341ca37ae35b80c
sahilsk11 <sahilkapur.a@gmail.com>
2024-07-25 19:01:37 -0400

    use cobra

--- cmd/script/main.go
+++ cmd/script/main.go
@@ -6,34 +6,94 @@ import (
 	"factorbacktest/cmd"
 	"factorbacktest/internal/domain"
 	"factorbacktest/internal/logger"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
+	"github.com/spf13/cobra"
 )
 
+var rootCmd = &cobra.Command{
+	Use:   "factorbacktest",
+	Short: "A tool for running various factor backtests.",
+	Long:  `A tool for running various factor backtests, with options for reconciliation and updating orders.`,
+}
+
+func init() {
+	rootCmd.AddCommand(reconcileCmd)
+	rootCmd.AddCommand(updateOrdersCmd)
+}
+
+var reconcileCmd = &cobra.Command{
+	Use:   "reconcile",
+	Short: "Reconcile the investment data",
+	Run: func(c *cobra.Command, args []string) {
+		handler, err := cmd.InitializeDependencies()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		profile, endProfile := domain.NewProfile()
+		defer endProfile()
+		ctx := context.WithValue(context.Background(), domain.ContextProfileKey, profile)
+
+		logger.Info("reconciling")
+
+		err = handler.InvestmentService.Reconcile(ctx)
+		if err != nil {
+			logger.Error(err)
+			log.Fatal(err)
+		}
+	},
+}
+
+var rebalanceCmd = &cobra.Command{
+	Use:   "rebalance",
+	Short: "Rebalance the investment data",
+	Run: func(c *cobra.Command, args []string) {
+		handler, err := cmd.InitializeDependencies()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		profile, endProfile := domain.NewProfile()
+		defer endProfile()
+		ctx := context.WithValue(context.Background(), domain.ContextProfileKey, profile)
+
+		logger.Info("rebalancing")
+
+		err = handler.InvestmentService.Rebalance(ctx)
+		if err != nil {
+			logger.Error(err)
+			log.Fatal(err)
+		}
+	},
+}
+
+var updateOrdersCmd = &cobra.Command{
+	Use:   "update-orders",
+	Short: "Update all pending orders",
+	Run: func(c *cobra.Command, args []string) {
+		handler, err := cmd.InitializeDependencies()
+		if err != nil {
+			log.Fatal(err)
+		}
+		logger.Info("updating orders")
+
+		updateOrders(handler)
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
 func main() {
-	handler, err := cmd.InitializeDependencies()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	profile, endProfile := domain.NewProfile()
-	defer endProfile()
-	ctx := context.WithValue(context.Background(), domain.ContextProfileKey, profile)
-
-	// err = handler.TradingService.Re()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// updateOrders(handler)
-
-	err = handler.InvestmentService.Reconcile(ctx)
-	if err != nil {
-		logger.Error(err)
-		log.Fatal(err)
-	}
-
+	Execute()
 }
 
 func updateOrders(handler *api.ApiHandler) {
--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/gocarina/gocsv v0.0.0-20240520201108-78e41c74b4b1
 	github.com/google/go-cmp v0.5.9
 	github.com/piquette/finance-go v1.1.0
+	github.com/spf13/cobra v1.8.1
 	go.uber.org/mock v0.4.0
 	go.uber.org/zap v1.13.0
 )
@@ -34,6 +35,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
@@ -43,6 +45,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	go.uber.org/atomic v1.6.0 // indirect
