70652af54d4d4de66d26de5d8cfcb4c2231ec9bc
sahilsk11 <sahilkapur.a@gmail.com>
2024-07-28 00:38:40 -0400

    move fb components to pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,13 @@
 import { useEffect, useState } from 'react';
-import BacktestChart from './pages/Backtest/BacktestChart';
-import FactorForm from "./pages/Backtest/Form";
-import Inspector from './pages/Backtest/FactorSnapshot';
-import BenchmarkManager from './pages/Backtest/BenchmarkSelector';
-import { BacktestInputs, BacktestSnapshot, GetSavedStrategiesResponse, GoogleAuthUser, LatestHoldings } from "./models";
-import { minMaxDates } from './util';
+import {  BacktestSnapshot, GoogleAuthUser } from "./models";
 import { v4 as uuidv4 } from 'uuid';
 import { ContactModal, HelpModal } from './common/Modals';
 import StatsFooter from './common/Footer';
 import { Nav } from './common/Nav';
 import styles from './App.module.css'
-import { useLocation, useNavigate } from 'react-router-dom';
+import FactorBacktestMain from 'pages/Backtest/Backtest';
+
+// we should move all contents from here to FactorBacktest or index
 
 export interface FactorData {
   name: string,
@@ -57,163 +54,6 @@ const App = ({ user, setUser }: { user: GoogleAuthUser | null, setUser: React.Di
   </>
 }
 
-function FactorBacktestMain({ userID, user, setUser }: {
-  userID: string
-  user: GoogleAuthUser | null,
-  setUser: React.Dispatch<React.SetStateAction<GoogleAuthUser | null>>,
-}) {
-  const [factorData, updateFactorData] = useState<FactorData[]>([]);
-  const [benchmarkData, updateBenchmarkData] = useState<BenchmarkData[]>([]);
-  const [inspectFactorDataIndex, updateInspectFactorDataIndex] = useState<number | null>(null);
-  const [inspectFactorDataDate, updateInspectFactorDataDate] = useState<string | null>(null);
-  const [latestHoldings, setLatestHoldings] = useState<LatestHoldings | null>(null);
-  const [assetUniverse, setAssetUniverse] = useState<string>("--");
-
-  const [bookmarked, setBookmarked] = useState(false);
-  const [savedStrategies, setSavedStrategies] = useState<GetSavedStrategiesResponse[]>([]);
-
-
-  // everything related to inputs pmuch
-  const [numSymbols, setNumSymbols] = useState(10);
-  const [factorExpression, setFactorExpression] = useState(`pricePercentChange(
-  nDaysAgo(7),
-  currentDate
-)`);
-  const [factorName, setFactorName] = useState("7_day_momentum_weekly");
-  const [backtestStart, setBacktestStart] = useState(twoYearsAgoAsString());
-  const [backtestEnd, setBacktestEnd] = useState(todayAsString());
-  const [samplingIntervalUnit, setSamplingIntervalUnit] = useState("monthly");
-  const [selectedFactor, setSelectedFactor] = useState("momentum");
-
-  const backtestInputs: BacktestInputs = {
-    factorName,
-    factorExpression,
-    backtestStart,
-    backtestEnd,
-    rebalanceInterval: samplingIntervalUnit,
-    numAssets: numSymbols,
-    assetUniverse,
-  }
-
-  const location = useLocation();
-  const pathname = location.pathname;
-  const navigate = useNavigate();
-
-  if (pathname === "/" && factorData.length > 0) {
-    navigate("/backtest")
-  } else if (pathname === "/backtest" && factorData.length === 0) {
-    navigate("/")
-  }
-
-  let takenNames: string[] = [];
-  factorData.forEach(fd => {
-    takenNames.push(fd.name)
-  })
-  benchmarkData.forEach(bd => {
-    takenNames.push(bd.symbol)
-  })
-  const { min: minFactorDate, max: maxFactorDate } = minMaxDates(factorData);
-
-  useEffect(() => {
-    if (factorData.length > 0) {
-      updateInspectFactorDataIndex(factorData.length - 1);
-      const d = factorData[factorData.length - 1].data;
-      const key = Object.keys(d).reduce((a, b) => (a > b ? b : a));
-      updateInspectFactorDataDate(key);
-    }
-  }, [factorData])
-
-  const updateFdIndex = (newVal: number) => {
-    updateInspectFactorDataIndex(newVal);
-    if (!inspectFactorDataDate || !factorData[newVal].data.hasOwnProperty(inspectFactorDataDate)) {
-      // todo - look for closest date instead of resetting
-      const date = Object.keys(factorData[newVal].data)[0];
-      updateInspectFactorDataDate(date);
-    }
-  }
-
-  const useVerboseBuilder = window.innerWidth > 767 && pathname !== "/backtest";
-
-  return (
-    <>
-      <div className={styles.my_container}>
-        <div className={`${styles.column} ${styles.form_wrapper}`}>
-          <FactorForm
-            // set this to the benchmark names that are already in used
-            user={user}
-            userID={userID}
-            takenNames={takenNames}
-            appendFactorData={(newFactorData: FactorData) => {
-              updateFactorData([...factorData, newFactorData])
-            }}
-            setUser={setUser}
-            fullscreenView={useVerboseBuilder}
-            setLatestHoldings={setLatestHoldings}
-            numSymbols={numSymbols}
-            setNumSymbols={setNumSymbols}
-            factorExpression={factorExpression}
-            setFactorExpression={setFactorExpression}
-            factorName={factorName}
-            setFactorName={setFactorName}
-            backtestStart={backtestStart}
-            setBacktestStart={setBacktestStart}
-            backtestEnd={backtestEnd}
-            setBacktestEnd={setBacktestEnd}
-            samplingIntervalUnit={samplingIntervalUnit}
-            setSamplingIntervalUnit={setSamplingIntervalUnit}
-            bookmarked={bookmarked}
-            setBookmarked={setBookmarked}
-            assetUniverse={assetUniverse}
-            setAssetUniverse={setAssetUniverse}
-            selectedFactor={selectedFactor}
-            setSelectedFactor={setSelectedFactor}
-            savedStrategies={savedStrategies}
-            setSavedStrategies={setSavedStrategies}
-          />
-          <BenchmarkManager
-            user={user}
-            userID={userID}
-            minDate={minFactorDate}
-            maxDate={maxFactorDate}
-            updateBenchmarkData={updateBenchmarkData}
-          />
-        </div>
-        {!useVerboseBuilder ?
-          <div id="backtest-chart" className={`${styles.column} ${styles.backtest_chart_container}`}>
-            <BacktestChart
-              benchmarkData={benchmarkData}
-              factorData={factorData}
-              updateInspectFactorDataIndex={updateFdIndex}
-              updateInspectFactorDataDate={updateInspectFactorDataDate}
-            />
-            <Inspector
-              fdIndex={inspectFactorDataIndex}
-              fdDate={inspectFactorDataDate}
-              factorData={factorData}
-              updateInspectFactorDataIndex={updateFdIndex}
-              updateInspectFactorDataDate={updateInspectFactorDataDate}
-              user={user}
-              latestHoldings={latestHoldings}
-              bookmarked={bookmarked}
-              setBookmarked={setBookmarked}
-              backtestInputs={backtestInputs}
-              setFactorName={setFactorName}
-              setSelectedFactor={setSelectedFactor}
-              setSavedStrategies={setSavedStrategies}
-              setUser={setUser}
-            />
-          </div> : null}
-      </div>
-    </>
-  )
-
-
-  // return (
-  //   <div className={styles.my_container}>
-  //     {useVerboseBuilder ? formComponent : classicView}
-  //   </div >
-  // );
-}
 
 export default App;
 
@@ -256,20 +96,3 @@ export function getOrCreateUserID(): string {
   return newUserID;
 }
 
-function todayAsString() {
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = String(today.getMonth() + 1).padStart(2, '0'); // Months are 0-based, so add 1
-  const day = String(today.getDate()).padStart(2, '0');
-
-  return `${year}-${month}-${day}`;
-}
-
-function twoYearsAgoAsString() {
-  const today = new Date();
-  const year = today.getFullYear() - 2;
-  const month = String(today.getMonth() + 1).padStart(2, '0'); // Months are 0-based, so add 1
-  const day = String(today.getDate()).padStart(2, '0');
-
-  return `${year}-${month}-${day}`;
-}
---
+++ frontend/src/pages/Backtest/Backtest.tsx
@@ -0,0 +1,187 @@
+import { FactorData, BenchmarkData } from "App";
+import { GoogleAuthUser, LatestHoldings, GetSavedStrategiesResponse, BacktestInputs } from "models";
+import { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { minMaxDates } from "../../util";
+import BacktestChart from "./BacktestChart";
+import BenchmarkManager from "./BenchmarkSelector";
+import Inspector from "./FactorSnapshot";
+import FactorForm from "./Form";
+import styles from 'App.module.css'
+
+
+export default function FactorBacktestMain({ userID, user, setUser }: {
+  userID: string
+  user: GoogleAuthUser | null,
+  setUser: React.Dispatch<React.SetStateAction<GoogleAuthUser | null>>,
+}) {
+  const [factorData, updateFactorData] = useState<FactorData[]>([]);
+  const [benchmarkData, updateBenchmarkData] = useState<BenchmarkData[]>([]);
+  const [inspectFactorDataIndex, updateInspectFactorDataIndex] = useState<number | null>(null);
+  const [inspectFactorDataDate, updateInspectFactorDataDate] = useState<string | null>(null);
+  const [latestHoldings, setLatestHoldings] = useState<LatestHoldings | null>(null);
+  const [assetUniverse, setAssetUniverse] = useState<string>("--");
+
+  const [bookmarked, setBookmarked] = useState(false);
+  const [savedStrategies, setSavedStrategies] = useState<GetSavedStrategiesResponse[]>([]);
+
+
+  // everything related to inputs pmuch
+  const [numSymbols, setNumSymbols] = useState(10);
+  const [factorExpression, setFactorExpression] = useState(`pricePercentChange(
+  nDaysAgo(7),
+  currentDate
+)`);
+  const [factorName, setFactorName] = useState("7_day_momentum_weekly");
+  const [backtestStart, setBacktestStart] = useState(twoYearsAgoAsString());
+  const [backtestEnd, setBacktestEnd] = useState(todayAsString());
+  const [samplingIntervalUnit, setSamplingIntervalUnit] = useState("monthly");
+  const [selectedFactor, setSelectedFactor] = useState("momentum");
+
+  const backtestInputs: BacktestInputs = {
+    factorName,
+    factorExpression,
+    backtestStart,
+    backtestEnd,
+    rebalanceInterval: samplingIntervalUnit,
+    numAssets: numSymbols,
+    assetUniverse,
+  }
+
+  const location = useLocation();
+  const pathname = location.pathname;
+  const navigate = useNavigate();
+
+  if (pathname === "/" && factorData.length > 0) {
+    navigate("/backtest")
+  } else if (pathname === "/backtest" && factorData.length === 0) {
+    navigate("/")
+  }
+
+  let takenNames: string[] = [];
+  factorData.forEach(fd => {
+    takenNames.push(fd.name)
+  })
+  benchmarkData.forEach(bd => {
+    takenNames.push(bd.symbol)
+  })
+  const { min: minFactorDate, max: maxFactorDate } = minMaxDates(factorData);
+
+  useEffect(() => {
+    if (factorData.length > 0) {
+      updateInspectFactorDataIndex(factorData.length - 1);
+      const d = factorData[factorData.length - 1].data;
+      const key = Object.keys(d).reduce((a, b) => (a > b ? b : a));
+      updateInspectFactorDataDate(key);
+    }
+  }, [factorData])
+
+  const updateFdIndex = (newVal: number) => {
+    updateInspectFactorDataIndex(newVal);
+    if (!inspectFactorDataDate || !factorData[newVal].data.hasOwnProperty(inspectFactorDataDate)) {
+      // todo - look for closest date instead of resetting
+      const date = Object.keys(factorData[newVal].data)[0];
+      updateInspectFactorDataDate(date);
+    }
+  }
+
+  const useVerboseBuilder = window.innerWidth > 767 && pathname !== "/backtest";
+
+  return (
+    <>
+      <div className={styles.my_container}>
+        <div className={`${styles.column} ${styles.form_wrapper}`}>
+          <FactorForm
+            // set this to the benchmark names that are already in used
+            user={user}
+            userID={userID}
+            takenNames={takenNames}
+            appendFactorData={(newFactorData: FactorData) => {
+              updateFactorData([...factorData, newFactorData])
+            }}
+            setUser={setUser}
+            fullscreenView={useVerboseBuilder}
+            setLatestHoldings={setLatestHoldings}
+            numSymbols={numSymbols}
+            setNumSymbols={setNumSymbols}
+            factorExpression={factorExpression}
+            setFactorExpression={setFactorExpression}
+            factorName={factorName}
+            setFactorName={setFactorName}
+            backtestStart={backtestStart}
+            setBacktestStart={setBacktestStart}
+            backtestEnd={backtestEnd}
+            setBacktestEnd={setBacktestEnd}
+            samplingIntervalUnit={samplingIntervalUnit}
+            setSamplingIntervalUnit={setSamplingIntervalUnit}
+            bookmarked={bookmarked}
+            setBookmarked={setBookmarked}
+            assetUniverse={assetUniverse}
+            setAssetUniverse={setAssetUniverse}
+            selectedFactor={selectedFactor}
+            setSelectedFactor={setSelectedFactor}
+            savedStrategies={savedStrategies}
+            setSavedStrategies={setSavedStrategies}
+          />
+          <BenchmarkManager
+            user={user}
+            userID={userID}
+            minDate={minFactorDate}
+            maxDate={maxFactorDate}
+            updateBenchmarkData={updateBenchmarkData}
+          />
+        </div>
+        {!useVerboseBuilder ?
+          <div id="backtest-chart" className={`${styles.column} ${styles.backtest_chart_container}`}>
+            <BacktestChart
+              benchmarkData={benchmarkData}
+              factorData={factorData}
+              updateInspectFactorDataIndex={updateFdIndex}
+              updateInspectFactorDataDate={updateInspectFactorDataDate}
+            />
+            <Inspector
+              fdIndex={inspectFactorDataIndex}
+              fdDate={inspectFactorDataDate}
+              factorData={factorData}
+              updateInspectFactorDataIndex={updateFdIndex}
+              updateInspectFactorDataDate={updateInspectFactorDataDate}
+              user={user}
+              latestHoldings={latestHoldings}
+              bookmarked={bookmarked}
+              setBookmarked={setBookmarked}
+              backtestInputs={backtestInputs}
+              setFactorName={setFactorName}
+              setSelectedFactor={setSelectedFactor}
+              setSavedStrategies={setSavedStrategies}
+              setUser={setUser}
+            />
+          </div> : null}
+      </div>
+    </>
+  )
+
+
+  // return (
+  //   <div className={styles.my_container}>
+  //     {useVerboseBuilder ? formComponent : classicView}
+  //   </div >
+  // );
+}
+
+function todayAsString() {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0'); // Months are 0-based, so add 1
+  const day = String(today.getDate()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+}
+
+function twoYearsAgoAsString() {
+  const today = new Date();
+  const year = today.getFullYear() - 2;
+  const month = String(today.getMonth() + 1).padStart(2, '0'); // Months are 0-based, so add 1
+  const day = String(today.getDate()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+}
