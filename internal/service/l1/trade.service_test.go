14527072b22469450b715fc861792b0ef3f3af4a
sahilsk11 <sahilkapur.a@gmail.com>
2024-07-28 15:44:03 -0400

    .

--- internal/service/l1/trade.service_test.go
+++ internal/service/l1/trade.service_test.go
@@ -1,14 +1,22 @@
 package l1_service
 
 import (
+	"context"
+	"database/sql"
 	"factorbacktest/internal/db/models/postgres/public/model"
+	"factorbacktest/internal/db/models/postgres/public/table"
 	"factorbacktest/internal/domain"
+	"factorbacktest/internal/repository"
 	mock_repository "factorbacktest/internal/repository/mocks"
 	"factorbacktest/internal/util"
 	"fmt"
 	"testing"
 	"time"
 
+	"github.com/alpacahq/alpaca-trade-api-go/v3/alpaca"
+	"github.com/go-jet/jet/v2/postgres"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/google/uuid"
 	_ "github.com/lib/pq"
 	"github.com/shopspring/decimal"
@@ -147,4 +155,218 @@ func TestAddTradesToPortfolio(t *testing.T) {
 		require.True(t, !ok, fmt.Sprintf("GOOG was found in portfolio positions: %v", newPortfolio.Positions["GOOG"]))
 		require.Equal(t, decimal.NewFromInt(-5_000), *newPortfolio.Cash)
 	})
+}
+
+func Test_tradeServiceHandler_ExecuteBlock(t *testing.T) {
+	t.Run("simple buys", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		db, err := util.NewTestDb()
+		require.NoError(t, err)
+		alpacaRepository := mock_repository.NewMockAlpacaRepository(ctrl)
+		tradeOrderRepository := mock_repository.NewMockTradeOrderRepository(ctrl)
+		handler := tradeServiceHandler{
+			Db:                   db,
+			AlpacaRepository:     alpacaRepository,
+			TradeOrderRepository: tradeOrderRepository,
+		}
+
+		// misc setup
+		tickerIDs := map[string]uuid.UUID{
+			"AAPL": uuid.New(),
+			"GOOG": uuid.New(),
+		}
+
+		ctx := context.Background()
+		rawTrades := []*domain.ProposedTrade{
+			{
+				Symbol:        "AAPL",
+				TickerID:      tickerIDs["AAPL"],
+				ExactQuantity: decimal.NewFromInt(100),
+				ExpectedPrice: decimal.NewFromInt(100),
+			},
+			{
+				Symbol:        "AAPL",
+				TickerID:      tickerIDs["AAPL"],
+				ExactQuantity: decimal.NewFromInt(100),
+				ExpectedPrice: decimal.NewFromInt(100),
+			},
+			{
+				Symbol:        "GOOG",
+				TickerID:      tickerIDs["GOOG"],
+				ExactQuantity: decimal.NewFromInt(100),
+				ExpectedPrice: decimal.NewFromInt(100),
+			},
+		}
+		rebalancerRunID := uuid.New()
+
+		// mocks
+		var expectedAaplOrder, expectedGoogOrder model.TradeOrder
+		{
+			// say we hold an arbitrarily large amount
+			// so we don't breach limits
+			alpacaRepository.EXPECT().
+				GetPositions().
+				Return([]alpaca.Position{
+					{
+						Symbol:       "AAPL",
+						Qty:          decimal.NewFromInt(5000),
+						QtyAvailable: decimal.NewFromInt(5000),
+					},
+					{
+						Symbol:       "GOOG",
+						Qty:          decimal.NewFromInt(5000),
+						QtyAvailable: decimal.NewFromInt(5000),
+					},
+				}, nil)
+
+			expectedAaplOrder = mockPlaceOrder(
+				t,
+				tradeOrderRepository,
+				alpacaRepository,
+				tickerIDs["AAPL"],
+				"AAPL",
+				nil,
+				decimal.NewFromInt(200),
+				model.TradeOrderSide_Buy,
+				alpaca.Buy,
+				rebalancerRunID,
+				decimal.NewFromInt(100),
+			)
+
+			expectedGoogOrder = mockPlaceOrder(
+				t,
+				tradeOrderRepository,
+				alpacaRepository,
+				tickerIDs["GOOG"],
+				"GOOG",
+				nil,
+				decimal.NewFromInt(100),
+				model.TradeOrderSide_Buy,
+				alpaca.Buy,
+				rebalancerRunID,
+				decimal.NewFromInt(100),
+			)
+		}
+
+		executedOrders, err := handler.ExecuteBlock(
+			ctx,
+			rawTrades,
+			rebalancerRunID,
+		)
+
+		require.NoError(t, err)
+		// kind of useless bc these are the same models
+		// you're mocking in the return
+		require.Equal(t, "", cmp.Diff(
+			[]model.TradeOrder{
+				expectedAaplOrder,
+				expectedGoogOrder,
+			},
+			executedOrders,
+			cmpopts.SortSlices(func(i, j model.TradeOrder) bool {
+				return i.TickerID.String() > j.TickerID.String()
+			}),
+		))
+	})
+}
+
+func mockPlaceOrder(
+	t *testing.T,
+	tradeOrderRepository *mock_repository.MockTradeOrderRepository,
+	alpacaRepository *mock_repository.MockAlpacaRepository,
+	tickerID uuid.UUID,
+	symbol string,
+	notes *string,
+	quantity decimal.Decimal,
+	dbSide model.TradeOrderSide,
+	alpacaSide alpaca.Side,
+	rebalancerRunID uuid.UUID,
+	expectedPrice decimal.Decimal,
+) model.TradeOrder {
+	tradeOrderID := uuid.New()
+	tradeOrderRepository.EXPECT().
+		Add(nil, gomock.Any()).
+		DoAndReturn(func(tx *sql.Tx, to model.TradeOrder) (*model.TradeOrder, error) {
+			expected := model.TradeOrder{
+				TickerID:          tickerID,
+				Side:              dbSide,
+				Status:            model.TradeOrderStatus_Error,
+				FilledQuantity:    decimal.Zero,
+				FilledPrice:       nil,
+				FilledAt:          nil,
+				Notes:             notes,
+				RebalancerRunID:   rebalancerRunID,
+				RequestedQuantity: quantity,
+				ExpectedPrice:     expectedPrice,
+			}
+			require.Equal(t, "", cmp.Diff(
+				expected,
+				to,
+				cmp.Comparer(func(i, j uuid.UUID) bool {
+					return i.String() == j.String()
+				}),
+			))
+
+			return &model.TradeOrder{
+				TradeOrderID: tradeOrderID,
+			}, nil
+		})
+
+	orderID := uuid.New()
+	alpacaRepository.EXPECT().
+		PlaceOrder(repository.AlpacaPlaceOrderRequest{
+			TradeOrderID: tradeOrderID,
+			Quantity:     quantity,
+			Symbol:       symbol,
+			Side:         alpacaSide,
+		}).Return(
+		&alpaca.Order{
+			ID: orderID.String(),
+		}, nil,
+	)
+
+	outputOrder := model.TradeOrder{
+		TradeOrderID:      tradeOrderID,
+		ProviderID:        &orderID,
+		TickerID:          tickerID,
+		Side:              dbSide,
+		Status:            model.TradeOrderStatus_Pending,
+		FilledQuantity:    quantity,
+		FilledPrice:       nil,
+		FilledAt:          nil,
+		CreatedAt:         time.Time{},
+		ModifiedAt:        time.Time{},
+		Notes:             notes,
+		RebalancerRunID:   rebalancerRunID,
+		RequestedQuantity: quantity,
+		ExpectedPrice:     expectedPrice,
+	}
+	tradeOrderRepository.EXPECT().
+		Update(nil, tradeOrderID, gomock.Any(), postgres.ColumnList{
+			table.TradeOrder.Status,
+			table.TradeOrder.ProviderID,
+			table.TradeOrder.FilledQuantity,
+			table.TradeOrder.FilledPrice,
+			table.TradeOrder.FilledAt,
+		}).DoAndReturn(func(tx *sql.Tx, tradeOrderID uuid.UUID, to model.TradeOrder, columns postgres.ColumnList) (*model.TradeOrder, error) {
+		expectedModel := model.TradeOrder{
+			Status:         model.TradeOrderStatus_Pending,
+			ProviderID:     &orderID,
+			FilledQuantity: decimal.Zero, // will probably be 0
+			FilledPrice:    nil,          // will probably be nil
+			FilledAt:       nil,          // will probably be nil
+		}
+
+		require.Equal(t, "", cmp.Diff(
+			expectedModel,
+			to,
+			cmp.Comparer(func(i, j uuid.UUID) bool {
+				return i.String() == j.String()
+			}),
+		))
+
+		return &outputOrder, nil
+	})
+
+	return outputOrder
 }
